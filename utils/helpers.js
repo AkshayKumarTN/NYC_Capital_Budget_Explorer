cd1d2a305e655165632af2212bea06c2aceee419
endiesworld <endiesworld@gmail.com>
2025-04-15 08:57:39 -0400

    feat: Added scripts to fetch data from NYC data repo, add new fields borough_full and neighborhood and populate mongodb database

--- utils/helpers.js
+++ utils/helpers.js
@@ -21,7 +21,7 @@ export function enrichData(item) {
     borough: boroughCode,
     borough_full: boroughMap[boroughCode] || "Unknown",
     council_district: isNaN(district) ? null : district,
-    neighborhoods: neighborhoodMap[key] || [],
+    borough_full: neighborhoodMap[key] || [],
     sponsor: item.sponsor || null,
     title: item.title || null,
     description: item.description || null,
