5d434e266fc1dc6cfb25803a9099b978117fa757
AKSHAY KUMAR TALUR NARASIMMULU <tnak369@gmail.com>
2025-04-27 14:02:40 -0400

    Updated export file names

--- routes/export.js
+++ routes/export.js
@@ -6,7 +6,7 @@ const router = Router();
 router.route('/csv').get(async (req, res) => {
   try {
     const filePath = await exportMethods.exportToCSV();
-    res.download(filePath, 'data.csv');
+    res.download(filePath, 'NYC_Capital_Budget_Projects.csv');
   } catch (e) {
     return res.status(500).json(e);
   }
@@ -17,7 +17,7 @@ router.route('/csv').get(async (req, res) => {
 router.route('/pdf').get(async (req, res) => {
   try {
     const filePath = await exportMethods.exportToPDF();
-    res.download(filePath, 'data.csv');
+    res.download(filePath, 'NYC_Capital_Budget_Projects.pdf');
   } catch (e) {
     return res.status(500).json(e);
   }
